0c4cc391f97e11feacb2c213162443dda7ec1946
atck3r <atck3r@localdomain>
2023-11-20 18:44:04 +0300

    Fixed

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,9 +4,11 @@ const host = 'localhost';
 const port = 1245;
 
 const app = http.createServer((_, res) => {
+  const text = 'Hello Holberton School!';
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+  res.setHeader('Content-Length', text.length);
+  res.end(text);
 });
 
 app.listen(port, host, () => {
