053c95175c8d35feeadfbf7d800227bbf6aaf963
atck3r <atck3r@localdomain>
2023-11-20 20:57:09 +0300

    Fixed

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const router = require('./routes');
+const router = require('./routes/index');
 
 const app = express();
 const port = 1245;
 
-app.use(router);
+app.use('/', indexRouter);
+app.use('/students', indexRouter);
+app.use('/students/:major', indexRouter);
 
 app.listen(port);
 
