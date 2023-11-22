2e988a361c5afe759f61360a5dfa07552d9528c7
atck3r <atck3r@localdomain>
2023-11-22 15:13:37 +0300

    Init

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const calculateNumber = require("./0-calcul.js");
+
+describe('calculateNumber', () => {
+  it('Two int number', () => {
+    assert.equal(calculateNumber(1, 3), 4);
+  });
+  it('One int and one float number', () => {
+    assert.equal(calculateNumber(1, 3.7), 5);
+  });
+  it('Two float number', () => {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+});
