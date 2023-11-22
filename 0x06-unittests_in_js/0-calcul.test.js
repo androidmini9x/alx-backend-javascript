a8846a456048032b45ab404be29b99e57f5808e8
atck3r <atck3r@localdomain>
2023-11-22 19:34:49 +0300

    Fixed

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,17 +1,25 @@
 const assert = require('assert');
-const calculateNumber = require("./0-calcul.js");
+const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
-  it('Two int number', () => {
-    assert.equal(calculateNumber(1, 3), 4);
+  it('round the first argument', () => {
+    assert.equal(calculateNumber(1.0, 0), 1);
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(1.7, 0), 2);
   });
-  it('One int and one float number', () => {
-    assert.equal(calculateNumber(1, 3.7), 5);
+
+  it('round the second argument', () => {
+    assert.equal(calculateNumber(0, 1.0), 1);
+    assert.equal(calculateNumber(0, 1.3), 1);
+    assert.equal(calculateNumber(0, 1.7), 2);
   });
-  it('Two float number', () => {
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-  });
-  it('Two float number', () => {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+
+  it('should return the right number', () => {
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(0, 1.2), 1);
+    assert.equal(calculateNumber(1.3, 1.3), 2);
+    assert.equal(calculateNumber(1.7, 1.2), 3);
+    assert.equal(calculateNumber(1.3, 1.8), 3);
+    assert.equal(calculateNumber(1.6, 1.8), 4);
   });
 });
